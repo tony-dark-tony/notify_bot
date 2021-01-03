3fd474c6e087e2e9043f8ce4a4e3ffbbc1088ccb
Final Bosss <68625695+tony-dark-tony@users.noreply.github.com>
2021-01-03 16:05:29 +0700

    Add somethings

--- public/js/client.js
+++ public/js/client.js
@@ -5,11 +5,11 @@ socket.on('hello', (data)=>{
 $(document).ready(function(){
     $('.contact1-form-btn').click(function(e){
         e.preventDefault();
-        socket.emit('msg', $('.input1').val());
+        socket.emit('message', $('.input1').val());
         $('.input1').val('');
     })
-    socket.on('message', (data) => {
-        $('.contact1-form-title').append('<span>'+data+'</span><br>');
-        console.log(data);
-    })
+    // socket.on('msg', (data) => {
+    //     $('.contact1-form-title').append('<span>'+data+'</span><br>');
+    //     console.log(data);
+    // })
 })
--- server.js
+++ server.js
@@ -9,14 +9,24 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.download("./text.txt");
 })
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('msg', (msg) => {
-        io.emit('message', msg)
+    console.log('A user connected');
+    socket.on('attime', (msg) => {
+        console.log(msg)
+    })
+    timeout();
+    socket.on('connection', (msg) => {
         console.log(msg)
     })
 });
+
+function timeout() {
+    setTimeout(function () {
+    io.emit('msg',"A message from server");
+      timeout();
+    }, 5000);
+  }
 http.listen(1212);
